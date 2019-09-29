52e2b3743b18ec3539854f4c5bc7f6cc5c18e94c
Tyler Johnson <tyler.johnson_2015@hotmail.com>
2019-09-28 23:54:54 -0400

    Add project files.

---
+++ CIS174Assignments/Controllers/AssignmentsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174Assignments.Models;
+using CIS174Assignments.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174Assignments.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        public IActionResult Assignment61(int accessLevel)
+        {
+            var students = new List<Students>()
+            {
+                new Students()
+                {
+                    FirstName = "Tyler",
+                    LastName = "Johnson",
+                    Grade = "A+"
+                },
+                new Students()
+                {
+                    FirstName = "Jane",
+                    LastName = "Rodriguez",
+                    Grade = "A"
+                },
+                new Students()
+                {
+                    FirstName = "Eva",
+                    LastName = "Johnson",
+                    Grade = "C+"
+                },
+                new Students()
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Grade = "B-"
+                },
+                new Students()
+                {
+                    FirstName = "Jennifer",
+                    LastName = "Williams",
+                    Grade = "C-"
+                }
+            };
+
+            StudentsViewModel viewModel = new StudentsViewModel();
+            viewModel.StudentList = students;
+            viewModel.AccessLevel = accessLevel;
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CIS174Assignments/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CIS174Assignments.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174Assignments.ViewModels
+{
+    public class StudentsViewModel
+    {
+        public List<Students> StudentList { get; set; }
+
+        public int AccessLevel { get; set; }
+
+    }
+}
